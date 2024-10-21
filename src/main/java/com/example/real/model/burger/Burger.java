f8b3c300735a4cdb317661be96e543b5f6fe04bc
Danilozxz <progdanilo@gmail.com>
2024-10-21 18:57:14 -0300

    feat: model burger

---
+++ src/main/java/com/example/real/model/burger/Burger.java
@@ -0,0 +1,25 @@
+package com.example.real.model.burger;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Burger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String name;
+    private double price;
+
+}
