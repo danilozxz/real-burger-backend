3a884d03ccb0665b257d3585b60644d3e46fa551
Danilozxz <progdanilo@gmail.com>
2024-10-21 19:17:29 -0300

    fix: modificação no tipo de geração de id

--- src/main/java/com/example/real/model/burger/Burger.java
+++ src/main/java/com/example/real/model/burger/Burger.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Setter
 public class Burger {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
