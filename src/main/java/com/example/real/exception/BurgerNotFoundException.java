1118e310cd43e458f0f1209c6a65a59d24b959c3
Danilozxz <progdanilo@gmail.com>
2024-10-24 18:23:30 -0300

    feat: exception para burger

---
+++ src/main/java/com/example/real/exception/BurgerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.real.exception;
+
+public class BurgerNotFoundException extends Exception {
+    public BurgerNotFoundException(String message) {
+        super(message);
+    }
+}
