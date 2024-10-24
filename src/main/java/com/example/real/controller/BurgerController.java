2a9298fc219751bf430cc918d207485bb77bf58c
Danilozxz <progdanilo@gmail.com>
2024-10-24 18:24:02 -0300

    feat: add controller do método update do burger

--- src/main/java/com/example/real/controller/BurgerController.java
+++ src/main/java/com/example/real/controller/BurgerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.real.dto.burger.BurgerDTO;
+import com.example.real.exception.BurgerNotFoundException;
 import com.example.real.model.burger.Burger;
 import com.example.real.service.BurgerService;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -35,5 +38,10 @@ public class BurgerController {
     @DeleteMapping("delete/{id}")
     public void deleteBurgerById(@PathVariable Long id) {
         service.deleteBurgerById(id);
+    }
+
+    @PutMapping("update/{id}") 
+    public Burger updateBurger(@PathVariable Long id, @RequestBody BurgerDTO burger) throws BurgerNotFoundException {
+        return service.updateBurger(id, burger);
     }
 }
