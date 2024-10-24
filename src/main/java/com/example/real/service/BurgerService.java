cfc7f45a90a0bface910c945dc9b3ae3a2475fbb
Danilozxz <progdanilo@gmail.com>
2024-10-24 18:23:00 -0300

    feat: add método update para burger

--- src/main/java/com/example/real/service/BurgerService.java
+++ src/main/java/com/example/real/service/BurgerService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.real.dto.burger.BurgerDTO;
+import com.example.real.exception.BurgerNotFoundException;
 import com.example.real.model.burger.Burger;
 import com.example.real.repository.BurgerRepository;
 
@@ -17,17 +19,38 @@ public class BurgerService {
         return repository.save(burger);
     }
 
-    public List<Burger> findAllBurgers () {
+    public List<Burger> findAllBurgers() {
         return repository.findAll();
     }
 
     public void deleteBurgerById(Long id) {
         Optional<Burger> opBurger = repository.findById(id);
 
-        if(opBurger.isEmpty()) {
+        if (opBurger.isEmpty()) {
             System.out.println("Não existe um burger com o id: " + id);
         }
 
         repository.deleteById(id);
     }
+
+    public Burger updateBurger(Long id, BurgerDTO burgerDTO) throws BurgerNotFoundException {
+        Burger existingBurger = repository.findById(id)
+                .orElseThrow(() -> new BurgerNotFoundException("Burger não encontrado"));
+
+        if (burgerDTO.name() != null) {
+            existingBurger.setName(burgerDTO.name());
+        }
+        if (burgerDTO.category() != null) {
+            existingBurger.setCategory(burgerDTO.category());
+        }
+        if (burgerDTO.ingredients() != null) {
+            existingBurger.setIngredients(burgerDTO.ingredients());
+        }
+        if (burgerDTO.price() != 0) { 
+            existingBurger.setPrice(burgerDTO.price());
+        }
+
+        return repository.save(existingBurger);
+    }
+
 }
